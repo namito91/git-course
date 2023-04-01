a0980abbcaa2572067df9cc12cc687d464778a32
doncorleone <patricionguirre@gmail.com>
2023-04-01 16:10:19 -0300

    added max element quant algorithm

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-console.log("hello git")
+let arr = [1,3,3,3,3,3,4,5,5,2];
+let cant = new Array(arr.length).fill(0);
 
+console.log(cant.length);
+        
+for (let index = 0; index < arr.length; index++) {
+  
+  cant[ arr[index]]++; 
+  
+}
+
+console.log(cant);
+ 
+
+ 
