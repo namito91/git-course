7c8b7fb7b26e5cfd8a753b23896a692c2534c441
doncorleone <patricionguirre@gmail.com>
2023-04-08 17:13:55 -0300

    added main.go file and added formating to app.js file

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
-let arr = [1,3,3,3,3,3,4,5,5,2];
+let arr = [1, 3, 3, 3, 3, 3, 4, 5, 5, 2];
 let cant = new Array(arr.length).fill(0);
 
 console.log(cant.length);
-        
+
 for (let index = 0; index < arr.length; index++) {
-  
-  cant[ arr[index]]++; 
-  
+  cant[arr[index]]++;
 }
 
 console.log(cant);
- 
-
- 
